a96afb68113a5c7bc28c3670b4d923920e3accbd
shubh4527 <75565285+shubh4527@users.noreply.github.com>
2021-07-25 17:22:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? shubham')
+  response.sendFile('index.html',{root: __dirname })
 }) 
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+
