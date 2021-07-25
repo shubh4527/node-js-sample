d4703432da64ead558ee83c3f8e14fd0cba268c7
shubh4527 <75565285+shubh4527@users.noreply.github.com>
2021-07-25 17:25:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.sendFile('index.html',{root: __dirname })
+  response.sendFile('./index.html',{root: __dirname })
 }) 
 
 app.listen(app.get('port'), function() {
